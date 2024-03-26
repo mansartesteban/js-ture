f79eaf88da6d1d2ec17ec199646a3ae263eaa0b8
Esteban Mansart <esteban.mansart@gmail.com>
2024-03-26 18:37:26 +0100

    Improving bundle build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,23 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import babel from "rollup-plugin-babel"
+import terser from '@rollup/plugin-terser'
 
 export default [
   {
     input: "src/index.js", // your entry point
-    output: {
-      name: "js-ture",
-      file: pkg.browser,
-      format: "umd",
-    },
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'cjs'
+      },
+      {
+        file: './dist/index.min.js',
+        format: 'iife',
+        name: 'version',
+        plugins: [terser()]
+      }
+    ],
     plugins: [
       resolve(),
       commonjs(),
@@ -19,4 +26,4 @@ export default [
       }),
     ],
   },
-];
+]
